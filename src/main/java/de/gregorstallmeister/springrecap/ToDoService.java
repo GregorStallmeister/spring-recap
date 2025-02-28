c022e09bb092322f16b180ebdc3de107d63d45bd
GregorStallmeister <post@gregor-stallmeister.de>
2025-02-28 09:49:44 +0100

    resolve warning

--- src/main/java/de/gregorstallmeister/springrecap/ToDoService.java
+++ src/main/java/de/gregorstallmeister/springrecap/ToDoService.java
@@ -3,7 +3,6 @@ package de.gregorstallmeister.springrecap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
